c3647d8ad024fca1e6abd62ab8ddc1fd47729098
Tobeyw <tobey1024@126.com>
2022-08-02 16:39:44 +0800

    update cros

--- internal/handler/getuserhandler.go
+++ internal/handler/getuserhandler.go
@@ -11,6 +11,7 @@ import (
 
 func GetUserHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		var req types.Address
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.Error(w, err)
