522966096de0044366b75df1b2c4407ac19f6d7e
Tobeyw <tobey1024@126.com>
2022-08-09 14:05:39 +0800

    redirect test3

--- internal/logic/twittercallbacklogic.go
+++ internal/logic/twittercallbacklogic.go
@@ -2,9 +2,6 @@ package logic
 
 import (
 	"context"
-	"database/sql"
-	"log"
-	"magaOasis/lib/type/nullstring"
 	"net/http"
 
 	"magaOasis/internal/svc"
@@ -29,33 +26,33 @@ func NewTwitterCallbackLogic(ctx context.Context, svcCtx *svc.ServiceContext) *T
 
 func (l *TwitterCallbackLogic) TwitterCallback(req *types.CallbackTwitterParam, w http.ResponseWriter, r *http.Request) {
 
-	url := "https://www.baidu.com/"
-
-	code := req.Code
-	address := req.State
-
-	accessToken, err := GetAccessTokenFromCode(code)
-	if err != nil {
-		//return &types.Response{"GetAccessTokenFromCode failed"}, err
-	}
-	userName, err := GetUserInfoTwitter(accessToken)
-	if err != nil {
-		//return &types.Response{"GetUserInfoTwitter failed"}, err
-	}
-
-	user, err := l.svcCtx.UserModel.FindOneByAddress(l.ctx, address)
-	if err != nil {
-		//return &types.Response{"FindUserByAddress failed"}, err
-	}
-	user.Twitter = sql.NullString{userName, nullstring.IsNull(userName)}
-
-	err = l.svcCtx.UserModel.Update(l.ctx, user)
-	if err != nil {
-		//return &types.Response{"UserInfoUpdate failed"}, err
-	}
-
-	log.Fatal(userName)
-
-	http.Redirect(w, r, url, http.StatusFound)
+	//url := "https://www.baidu.com/"
+	//
+	//code := req.Code
+	//address := req.State
+	//
+	//accessToken, err := GetAccessTokenFromCode(code)
+	//if err != nil {
+	//	//return &types.Response{"GetAccessTokenFromCode failed"}, err
+	//}
+	//userName, err := GetUserInfoTwitter(accessToken)
+	//if err != nil {
+	//	//return &types.Response{"GetUserInfoTwitter failed"}, err
+	//}
+	//
+	//user, err := l.svcCtx.UserModel.FindOneByAddress(l.ctx, address)
+	//if err != nil {
+	//	//return &types.Response{"FindUserByAddress failed"}, err
+	//}
+	//user.Twitter = sql.NullString{userName, nullstring.IsNull(userName)}
+	//
+	//err = l.svcCtx.UserModel.Update(l.ctx, user)
+	//if err != nil {
+	//	//return &types.Response{"UserInfoUpdate failed"}, err
+	//}
+	//
+	//log.Fatal(userName)
+	//
+	//http.Redirect(w, r, url, http.StatusFound)
 
 }
