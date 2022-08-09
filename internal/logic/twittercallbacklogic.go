14287b9fa61d21adf850418531d9f94cd3997033
Tobeyw <tobey1024@126.com>
2022-08-09 14:14:55 +0800

    redirect test3

--- internal/logic/twittercallbacklogic.go
+++ internal/logic/twittercallbacklogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"database/sql"
+	"log"
+	"magaOasis/lib/type/nullstring"
 	"net/http"
 
 	"magaOasis/internal/svc"
@@ -27,32 +30,36 @@ func NewTwitterCallbackLogic(ctx context.Context, svcCtx *svc.ServiceContext) *T
 func (l *TwitterCallbackLogic) TwitterCallback(req *types.CallbackTwitterParam, w http.ResponseWriter, r *http.Request) {
 
 	url := "https://www.baidu.com/"
-	//
-	//code := req.Code
-	//address := req.State
-	//
-	//accessToken, err := GetAccessTokenFromCode(code)
-	//if err != nil {
-	//	//return &types.Response{"GetAccessTokenFromCode failed"}, err
-	//}
-	//userName, err := GetUserInfoTwitter(accessToken)
-	//if err != nil {
-	//	//return &types.Response{"GetUserInfoTwitter failed"}, err
-	//}
-	//
-	//user, err := l.svcCtx.UserModel.FindOneByAddress(l.ctx, address)
-	//if err != nil {
-	//	//return &types.Response{"FindUserByAddress failed"}, err
-	//}
-	//user.Twitter = sql.NullString{userName, nullstring.IsNull(userName)}
-	//
-	//err = l.svcCtx.UserModel.Update(l.ctx, user)
-	//if err != nil {
-	//	//return &types.Response{"UserInfoUpdate failed"}, err
-	//}
-	//
-	//log.Fatal(userName)
-	//
+
+	code := req.Code
+	address := req.State
+
+	accessToken, err := GetAccessTokenFromCode(code)
+	if err != nil {
+		log.Println("GetAccessTokenFromCode failed ", err)
+		//return &types.Response{"GetAccessTokenFromCode failed"}, err
+	}
+	userName, err := GetUserInfoTwitter(accessToken)
+	if err != nil {
+		log.Println("GetUserInfoTwitter failed ", err)
+		//return &types.Response{"GetUserInfoTwitter failed"}, err
+	}
+
+	user, err := l.svcCtx.UserModel.FindOneByAddress(l.ctx, address)
+	if err != nil {
+		log.Println("FindUserByAddress failed ", err)
+		//return &types.Response{"FindUserByAddress failed"}, err
+	}
+	user.Twitter = sql.NullString{userName, nullstring.IsNull(userName)}
+
+	err = l.svcCtx.UserModel.Update(l.ctx, user)
+	if err != nil {
+		log.Println("UserInfoUpdate failed ", err)
+		//return &types.Response{"UserInfoUpdate failed"}, err
+	}
+
+	log.Fatal(userName)
+
 	http.Redirect(w, r, url, http.StatusFound)
 
 }
