9b060615d4c5405b92aec13b00c4f3b8fb17a156
Tobeyw <tobey1024@126.com>
2022-08-10 14:53:45 +0800

    add start

---
+++ src/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+type Config struct {
+	rest.RestConf
+	User           zrpc.RpcClientConf
+	DataSourceTest string
+	DataSourceMain string
+	Table          string
+	Cache          cache.CacheConf
+	Email          struct {
+		Account string
+		Passwd  string
+		Host    string
+		Port    int
+	}
+	MongoDBTest string
+	DBTest      string
+	MongoDBMain string
+	DBMain      string
+}
---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo shut down existed docker service
+echo you env is $1
+if [ $1 == "TEST" ]
+then
+    export RUNTIME="test"
+    docker stop megaoasis_profie_test
+    docker stop megaoasis_email_test
+
+    docker container rm megaoasis_profie_test
+    docker container rm megaoasis_email_test
+
+    docker rmi test_megaoasis_profie -f
+    docker rmi megaoasis_ws -f
+    docker-compose -p "test" up -d
+fi
+
+if [ $1 == "STAGING" ]
+then
+    export RUNTIME="main"
+    docker stop megaoasis_profie_staging
+    docker stop megaoasis_email_staging
+
+    docker container rm megaoasis_profie_staging
+    docker container rm megaoasis_email_staging
+
+    docker rmi staging_megaoasis_profie -f
+    docker rmi staging_megaoasis_email -f
+    docker-compose -p "main" up -d
+fi
+
+
