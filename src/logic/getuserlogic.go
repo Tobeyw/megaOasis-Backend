d0dd1199a011f6d09049a19d6104e45e34f95d3b
Tobeyw <tobey1024@126.com>
2022-08-10 18:36:54 +0800

    update config

--- src/logic/getuserlogic.go
+++ src/logic/getuserlogic.go
@@ -25,6 +25,8 @@ func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLo
 }
 
 func (l *GetUserLogic) GetUser(req *types.Address) (resp *types.UserResp, err error) {
+	fmt.Println("==========================================")
+	fmt.Println(req.Address)
 	res, err := l.svcCtx.UserModel.FindOneByAddress(l.ctx, req.Address)
 	fmt.Println(err)
 	if err != nil && err != sqlc.ErrNotFound {
